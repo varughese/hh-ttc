289a17a059eef653e84a68f139763a2d96ad3e01
BuildTools <j26walker@gmail.com>
2016-07-07 17:35:08 -0400

    ng-repeat in events table

--- public/controllers/dashboard.js
+++ public/controllers/dashboard.js
@@ -1,7 +1,22 @@
 angular.module('nhs')
 
 .controller('dashboard', ['$scope', "$state", "$rootScope", function($scope, $state, $rootScope){
-    
+    $scope.events = [
+        {
+            _id: 1,
+            name: "Special Olympics",
+            date: "5/29/2016",
+            hours: 10,
+            checked: false 
+        },
+        {
+            _id: 2,
+            name: "Blood Drive",
+            date: "5/16/2015",
+            hours: 3,
+            checked: false 
+        },
+    ];
 }])
 
 ;
