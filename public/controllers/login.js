e6405757eeecff6e6bdd05bc6000d6a5a4f991ad
BuildTools <j26walker@gmail.com>
2016-07-07 17:09:15 -0400

    users service

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -2,11 +2,11 @@ angular.module('nhs')
 
 .controller('login', ['$scope', "$state", function($scope, $state){
     $scope.login = function(){
-    $scope.user = {
-        loggedIn: true,
-        username: varhawk,
-        password: deeznuts
-    };
+        $scope.user = {
+            loggedIn: true,
+            username: "jason",
+            password: "deeznuts"
+        };
 
         $state.go('dashboard');
     };
---
+++ public/services/users.js
@@ -0,0 +1,11 @@
+angular.module('nhs')
+
+.service("users", function(){
+    var users = {
+        username: '',
+        password: '',
+        loggedIn: false
+    };
+})
+
+;
