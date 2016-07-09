b46071b17cc14e65720c971a8ffd7032b3df70d1
Mathew Varughese <mat@imginconline.com>
2016-07-08 23:43:19 -0400

    Refactor all event routes to diff file

--- server/routes/api.js
+++ server/routes/api.js
@@ -140,74 +140,12 @@ module.exports = function(app, express) {
         });
     });
 
-    apiRouter.route('/users/:userID/events')
-        .get(function(req, res) {
-            req.user.populate('events', function(err, user) {
-                if (err) return res.send(err);
-
-                res.json(user.events);
-            });
-        })
-        .post(function(req, res) {
-            var event = new Event();
-            event.name = req.body.name;
-            event.date = new Date(req.body.date);
-            event.hours = req.body.hours;
-            event.user = req.user;
-
-            event.save(function(err, event) {
-                if(err) return res.send(err);
-
-                req.user.events.push(event);
-                req.user.save(function(err, user) {
-                    if(err) return res.send(err);
-                    res.json(user);
-                });
-            });
-        });
-
-    apiRouter.route('/users/:userID/events/:eventID')
-        .get(function(req, res) {
-            Event.findById(req.params.eventID, function(err, evt) {
-                res.json(evt);
-            });
-        })
-        .put(function(req, res) {
-            Event.findById(req.params.eventID, function(err, event) {
-                if(err) return res.send(err);
-
-                if(req.body.name) event.name = req.body.name;
-                if(req.body.hours) event.hours = req.body.hours;
-                if(req.body.date) event.date = new Date(req.body.date);
-                if(req.body.checked) event.checked = req.body.checked;
-
-                event.save(function(err) {
-                    if(err) res.send(err);
-
-                    res.json({message: 'Event updated'});
-                });
-            });
-        })
-        .delete(function(req, res) {
-            Event.remove({
-                _id: req.params.eventID
-            }, function(err, user) {
-                if(err) return res.send(err);
-                res.json({message: 'succesfully deleted'});
-            });
-        });
 
     apiRouter.get('/me', function(req, res) {
         res.send(req.decoded);
     });
 
-    apiRouter.get('/upcoming-events', function(req, res) {
-        UpcomingEvent.find(function(err, uevents) {
-            if(err) res.send(err);
-
-            res.json(uevents);
-        });
-    });
+    require("./events")(apiRouter);
 
     require("./admin")(apiRouter);
 
