d0b926ced82c7f9a854c0914a3a87711e89706c4
Jason Walker <j26walker@gmail.com>
2016-08-22 19:10:22 -0400

    remove flicker of navbar

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -20,15 +20,11 @@ angular.module('nhs')
 }])
 
 .controller('signup', ['$scope', "$state", "$rootScope", "User", function($scope, $state, $rootScope, User){
-    $scope.processing = false;
-
-    if(User.loggedIn()) $state.go("dashboard");
 
     $scope.signup = function(){
 		console.log($scope.signup);
-		$scope.processing = true;
 
-        User.login($scope.user.username.toLowerCase(), $scope.user.password)
+        User.create($scope.user.username.toLowerCase(), $scope.user.password)
             .then(function(user) {
                 $rootScope.user = user;
                 $state.go('dashboard');
