877bd2af8e8395b22396b06e411fa1e682f9f7b3
Mathew Varughese <mat@imginconline.com>
2016-09-21 15:43:13 -0400

    linkified the description

--- public/controllers/dashboard.js
+++ public/controllers/dashboard.js
@@ -1,6 +1,6 @@
 angular.module('nhs')
 
-.controller('dashboard', ['$scope', "$state", "$rootScope", "Event", "UpcomingEvent", "$timeout", function($scope, $state, $rootScope, Event, UpcomingEvent, $timeout){
+.controller('dashboard', ['$scope', "$state", "$rootScope", "Event", "UpcomingEvent", "$timeout", "$sce", function($scope, $state, $rootScope, Event, UpcomingEvent, $timeout, $sce){
     $scope.events = [];
 
     var watcher = $rootScope.$watch("user", function(n, o, s) {
@@ -16,6 +16,28 @@ angular.module('nhs')
 
     UpcomingEvent.all()
         .then(function(upcoming) {
+            for(var i=0; i<upcoming.length; i++) {
+                var u = upcoming[i],
+                    desc = u.description;
+
+                if(desc) {
+                    var http = desc.toLowerCase().indexOf("http");
+
+                    if(http > -1) {
+                        var hrefText = "Link",
+                            space = desc.indexOf(" ", http);
+
+                        if(space < 0) {
+                            space = desc.length;
+                        }
+                        
+                        var html = "<a target=\"_blank\" href=\"" + desc.substring(http, space) + "\">Link</a>";
+                        desc = desc.substring(0, http) + html + desc.substring(space);
+                    }
+
+                }
+                u.htmlDescription = $sce.trustAsHtml(desc);
+            }
             $rootScope.upcoming = upcoming;
         });
 
