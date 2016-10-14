3506f6fae69ca76a5ac9cfecb05a5ff5fe11c11e
Mathew Varughese <mat@imginconline.com>
2016-10-13 22:22:48 -0400

    Rebrand

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 8080,
-    database: 'mongodb://varughese:Blackjack21@ds011745.mlab.com:11745/hhnhs',
+    database: 'mongodb://wang:ttc@ds053166.mlab.com:53166/hhttc',
     secret: 'jawnjawnjawnjawn'
 };
